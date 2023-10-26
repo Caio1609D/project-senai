daa2a5cf67411c1f00bca6cb668b4ba538034fc0
Caio1609D <87949006+Caio1609D@users.noreply.github.com>
2023-10-26 14:38:50 +0000

    Operações com o Banco de Dados

--- tools/db.py
+++ tools/db.py
@@ -2,6 +2,9 @@ import sqlalchemy
 from sqlalchemy.orm import Session, Mapped, DeclarativeBase, mapped_column
 
 engine = sqlalchemy.create_engine("sqlite:///reagents.db")
+insert = sqlalchemy.insert
+select = sqlalchemy.select
+update = sqlalchemy.update
 
 class Base(DeclarativeBase):
     pass
@@ -9,5 +12,25 @@ class Base(DeclarativeBase):
 class Reagent(Base):
     __tablename__ = "reagents"
 
-    id: Mappend[int] = mapped_column(primary_key=True)
-    name: Mappend[str] = mapped_column()
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    formula: Mapped[str] = mapped_column()
+    density: Mapped[float] = mapped_column()
+    quantity: Mapped[float] = mapped_column()
+    state: Mapped[str] = mapped_column()
+
+    def __repr__(self) -> str:
+        return f"Reagent(id={self.id!r}, name={self.name!r}, formula={self.formula!r}, density={self.density!r}, quantity={self.quantity!r})"
+    
+Base.metadata.create_all(engine)
+
+def add_reagent(name, formula, density, quantity):
+    with Session(engine) as session:
+        session.execute(insert(Reagent), {"name": name, "formula": formula, "density": density, "quantity": quantity})
+        session.commit()
+
+def update_reagent(rid, quantity):
+    with Session(engine) as session:
+        session.execute(update(Reagent).where(Reagent.id == rid).values(quantity=quantity))
+        session.commit()
+
