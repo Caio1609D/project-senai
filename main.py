306ddf4c6b54db2998c7764196b87bf9cf09d974
unknown <caiooliveiradev@gmail.com>
2023-11-15 23:22:11 -0300

    Mudando a abordagem do Tkinter para uma mais fácil

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 import sys
 sys.path.append('/tools')
 import tools.db as db
 
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
+root = tk.Tk()
+style = ttk.Style()
 
-def main():
-    ttk.Label(frm, text="Opa, bom dia").grid(column=0, row=0)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=0)
-    root.mainloop()
+class Application():
+    
+    def __init__(self):
+        self.root = root
+        self.style = style
+        self.screen()
+        self.frames()
+        root.mainloop()
+    
+    def screen(self):
+        root = self.root
+        root.geometry("1800x720")
+        root.minsize(width=400, height=400)
+        root.configure(background="#224c94")
+    
+    def visuals(self):
+        style = self.style
+        style.configure("BW.TLabel", foreground="white", background="#224c94")
 
-main()
+    def frames(self):
+        self.frm = tk.Frame(root, bd=4, bg="#ebf2ff", highlightbackground="#527aff", highlightthickness=3)
+        self.frm.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.48)
+
+    def buttons(self):
+        self.clean = tk.Button(self.frm)
+
+Application()
