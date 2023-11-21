e2471cb735c0f6c4068cebf9ba74e0b40ea83444
unknown <caiooliveiradev@gmail.com>
2023-11-21 17:08:30 -0300

    Adding functions

--- main.py
+++ main.py
@@ -12,19 +12,30 @@ class Funcs():
 
     # Responsável por puxar os reagentes do db e transformar em tabela
     def refresh_table(self):
+        for i in self.tv.get_children():
+            self.tv.delete(i)
+        
         for i in db.list_reagents():
-            self.tv.insert(parent='', index=0, iid=0, text='', values=(
+            self.tv.insert(parent='', index=len(self.tv.get_children()), iid=len(self.tv.get_children()), text='', values=(
+                i.id,
                 i.name, 
                 i.formula, 
                 i.density, 
                 i.quantity, 
-                "Disponível" if i.state == "available" else "Indisponível"
+                "Disponível" if i.state == "available" else "Indisponível",
             ))
 
     def select_row(self, event):
         curItem = self.tv.focus()
         self.selected = self.tv.item(curItem)["values"]
-        print(self.selected)
+
+    def get_reagent(self, rid):
+        db.get_reagent(rid)
+        self.refresh_table()
+
+    def return_reagent(self, rid, quantity):
+        db.return_reagent(rid, quantity)
+        self.refresh_table()
 
 
 # Classe que repersenta a nossa aplicação
@@ -37,6 +48,7 @@ class Application(Funcs):
         self.screen()
         self.frames()
         self.treeview()
+        self.widgets()
         root.mainloop()
     
     # Configuração da tela
@@ -52,35 +64,51 @@ class Application(Funcs):
 
     # Configuração dos Frames internos
     def frames(self):
-        self.frm = tk.Frame(root, bd=4, bg="#ebf2ff", highlightbackground="#527aff", highlightthickness=3)
-        self.frm.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.48)
+        self.frm1 = tk.Frame(root, bd=4, bg="#ebf2ff", highlightbackground="#527aff", highlightthickness=3)
+        self.frm1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.48)
+
+        self.frm2 = tk.Frame(root, bd=4, bg="#ebf2ff", highlightbackground="#527aff", highlightthickness=3)
+        self.frm2.place(relx=0.02, rely=0.52, relwidth=0.96, relheight=0.48)
 
     def widgets(self):
-        self.cleanBtn = tk.Button(self.frm)
+        # Botão para pegar (?) um reagente
+        self.getBtn = tk.Button(self.frm2, text="Pegar", command=lambda: self.get_reagent(self.selected[0])) # Achar um termo/legenda melhor que "pegar"
+        self.getBtn.place(relx=0.05, rely=0.1, relwidth=0.1, relheight=0.1)
+
+        # Botão pra falar a quantidade de reagente devolvido
+        self.returnEntry = tk.Entry(self.frm2)
+        self.returnEntry.place(relx=0.175, rely=0.1, relwidth=0.1, relheight=0.1)
+
+        # Botão para devolver um reagente
+        self.returnBtn = tk.Button(self.frm2, text="Devolver", command=lambda: self.return_reagent(self.selected[0], float(self.returnEntry.get())))
+        self.returnBtn.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.1)
 
     # Configuração da Treeview
     def treeview(self):
         # Cria as colunas da Treeview
-        self.tv = ttk.Treeview(self.frm)
-        self.tv['columns'] = ('Name', 'Formula', 'Density', 'Quantity', 'State')
+        self.tv = ttk.Treeview(self.frm1)
+        self.tv['columns'] = ('id', 'name', 'formula', 'density', 'quantity', 'state')
+        self.tv["displaycolumns"] = ['name', 'formula', 'density', 'quantity', 'state'] 
         self.tv.column('#0', width=0, stretch=tk.NO)
-        self.tv.column('Name', anchor=tk.CENTER, width=150)
-        self.tv.column('Formula', anchor=tk.CENTER, width=150)
-        self.tv.column('Density', anchor=tk.CENTER, width=50)
-        self.tv.column('Quantity', anchor=tk.CENTER, width=50)
-        self.tv.column('State', anchor=tk.CENTER, width=100)
+        self.tv.column('id', width=0, stretch=tk.NO)
+        self.tv.column('name', anchor=tk.CENTER, width=150)
+        self.tv.column('formula', anchor=tk.CENTER, width=150)
+        self.tv.column('density', anchor=tk.CENTER, width=50)
+        self.tv.column('quantity', anchor=tk.CENTER, width=50)
+        self.tv.column('state', anchor=tk.CENTER, width=100)
 
         # Adiciona os titulos às colunas
         self.tv.heading('#0', text='', anchor=tk.CENTER)
-        self.tv.heading('Name', text='Nome', anchor=tk.CENTER)
-        self.tv.heading('Formula', text='Fórmula', anchor=tk.CENTER)
-        self.tv.heading('Density', text='Densidade', anchor=tk.CENTER)
-        self.tv.heading('Quantity', text='Quantidade', anchor=tk.CENTER)
-        self.tv.heading('State', text='Estado', anchor=tk.CENTER)
+        self.tv.heading('id', text='', anchor=tk.CENTER)
+        self.tv.heading('name', text='Nome', anchor=tk.CENTER)
+        self.tv.heading('formula', text='Fórmula', anchor=tk.CENTER)
+        self.tv.heading('density', text='Densidade', anchor=tk.CENTER)
+        self.tv.heading('quantity', text='Quantidade', anchor=tk.CENTER)
+        self.tv.heading('state', text='Estado', anchor=tk.CENTER)
         self.tv.bind('<<TreeviewSelect>>', self.select_row)
 
         # Cria a Scroolbar
-        self.scroll = tk.Scrollbar(self.frm, orient='vertical')
+        self.scroll = tk.Scrollbar(self.frm1, orient='vertical')
         
         # Adiciona os reagentes como linha
         self.refresh_table()
